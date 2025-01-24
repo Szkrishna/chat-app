56febee6afb05cddfa8a7f5d6516f3c40d323ced
Szkrishna <krisnas9792@gmail.com>
2025-01-24 23:33:01 +0530

    created register, login and chat component and using it in app.js inside browser router

--- public/src/App.js
+++ public/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Chat from './pages/Chat';
 
 export default function App() {
   return (
-    <div>
-      Chat App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={ <Register/> }></Route>
+        <Route path="/login" element={ <Login/> }></Route>
+        <Route path="/chat" element={ <Chat/> }></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
