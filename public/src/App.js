8794192cd8175cb1bf7a1160c2a08011616774d5
Szkrishna <krisnas9792@gmail.com>
2025-01-24 23:58:24 +0530

    creating register form in register component

--- public/src/App.js
+++ public/src/App.js
@@ -10,7 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/register" element={ <Register/> }></Route>
         <Route path="/login" element={ <Login/> }></Route>
-        <Route path="/chat" element={ <Chat/> }></Route>
+        <Route path="/" element={ <Chat/> }></Route>
       </Routes>
     </BrowserRouter>
   )
--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 function Register() {
-  return (
-    <div>Register</div>
-  )
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert("form");
+    }
+    const handleChange = (event) => {
+
+    }
+    return (
+        <><formContainer>
+            <form onSubmit={(event) => handleSubmit(event)}>
+                <div className="brand">
+                    <img src="" alt="" />
+                    <h1>snappy</h1>
+                </div>
+                <input type="text" placeholder='Username' name="username" onChange={(e) => handleChange(e)} />
+                <input type="email" placeholder='Email' name="email" onChange={(e) => handleChange(e)} />
+                <input type="password" placeholder='Password' name="password" onChange={(e) => handleChange(e)} />
+                <input type="password" placeholder='Confirm Password' name="confirmPass" onChange={(e) => handleChange(e)} />
+                <button type="submit">Create User</button>
+                <span>Already have an account ? <Link to="/login">Login</Link></span>
+            </form>
+        </formContainer>
+        </>
+    )
 }
+
+const formContainer = styled.div``;
 
 export default Register;
