2f851c80b8062811f0c965bc137b5513222c3040
Szkrishna <krisnas9792@gmail.com>
2025-01-25 22:57:45 +0530

    added toastr in the form, toastr container and use validation to the form

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,15 +1,63 @@
 import React from 'react';
+import { useState, useEffect } from "react"
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import Logo from "../assets/logo.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Register() {
+    const toastOptions = {
+        position: "bottom-right",
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "dark",
+    };
+
+    const [values, setvalues] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    });
+
+    const handleValidation = () => {
+        const { password, confirmPassword, username, email } = values;
+        if (password !== confirmPassword) {
+            toast.error(
+                "Password and confirm password should be same.",
+                toastOptions
+            );
+            return false;
+        } else if (username.length < 3) {
+            toast.error(
+                "Username should be greater than 3 characters.",
+                toastOptions
+            );
+            return false;
+        } else if (password.length < 8) {
+            toast.error(
+                "Password should be equal or greater than 8 characters.",
+                toastOptions
+            );
+            return false;
+        } else if (email === "") {
+            toast.error("Email is required.", toastOptions);
+            return false;
+        }
+
+        return true;
+    };
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert("form");
+        console.log(toast, { "form value": { [event.target.name]: event.target.value } })
+        handleValidation();
     }
-    const handleChange = (event) => {
 
+    const handleChange = (event) => {
+        setvalues({ ...values, [event.target.name]: event.target.value });
     }
     return (
         <>
@@ -49,6 +97,7 @@ function Register() {
                     </span>
                 </form>
             </FormContainer>
+            <ToastContainer />
         </>
     )
 }
