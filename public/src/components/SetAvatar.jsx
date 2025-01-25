e9726eb311560464157ebbf58581d6e80f1c5b86
Szkrishna <krisnas9792@gmail.com>
2025-01-26 02:55:17 +0530

    added css for set avater component

--- public/src/components/SetAvatar.jsx
+++ public/src/components/SetAvatar.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { Buffer } from "buffer";
 import axios from "axios";
+import { Buffer } from "buffer";
 import loader from "../assets/loader.gif";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
 import { setAvatarRoute } from "../utils/APIRoutes";
-
-function SetAvatar() {
+export default function SetAvatar() {
     const api = `https://api.multiavatar.com/4645646`;
     const navigate = useNavigate();
     const [avatars, setAvatars] = useState([]);
@@ -30,7 +29,6 @@ function SetAvatar() {
         };
         checkUser();
     }, [navigate]);
-    
 
     const setProfilePicture = async () => {
         if (selectedAvatar === undefined) {
@@ -59,34 +57,21 @@ function SetAvatar() {
     };
 
     useEffect(() => {
-        let isMounted = true;
-
         const fetchAvatars = async () => {
             const data = [];
             for (let i = 0; i < 4; i++) {
-                try {
-                    const response = await axios.get(
-                        `${api}/${Math.round(Math.random() * 1000)}`
-                    );
-                    const buffer = new Buffer(response.data);
-                    if (isMounted) {
-                        data.push(buffer.toString("base64"));
-                    }
-                } catch (err) {
-                    console.error("Error fetching avatar:", err);
-                }
+                const image = await axios.get(
+                    `${api}/${Math.round(Math.random() * 1000)}`
+                );
+                const buffer = Buffer.from(image.data); // Use `Buffer.from` for Node.js Buffer
+                data.push(buffer.toString("base64"));
             }
-            if (isMounted) {
-                setAvatars(data);
-                setIsLoading(false);
-            }
-        }; 
-        fetchAvatars();
-
-        return () => {
-            isMounted = false;
+            setAvatars(data);
+            setIsLoading(false);
         };
-    }, []);  
+        fetchAvatars();
+    }, []);
+
     return (
         <>
             {isLoading ? (
@@ -122,9 +107,61 @@ function SetAvatar() {
                 </Container>
             )}
         </>
-    )
+    );
 }
 
-const Container = styled.div``
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 3rem;
+  background-color: #131324;
+  height: 100vh;
+  width: 100vw;
 
-export default SetAvatar;
+  .loader {
+    max-inline-size: 100%;
+  }
+
+  .title-container {
+    h1 {
+      color: white;
+    }
+  }
+  .avatars {
+    display: flex;
+    gap: 2rem;
+
+    .avatar {
+      border: 0.4rem solid transparent;
+      padding: 0.4rem;
+      border-radius: 5rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      transition: 0.5s ease-in-out;
+      img {
+        height: 6rem;
+        transition: 0.5s ease-in-out;
+      }
+    }
+    .selected {
+      border: 0.4rem solid #4e0eff;
+    }
+  }
+  .submit-btn {
+    background-color: #4e0eff;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #4e0eff;
+    }
+  }
+`;
