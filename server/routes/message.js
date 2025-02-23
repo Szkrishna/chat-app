f91c6645b21457d0a44900c7feb6fae1edc56577
Szkrishna <krisnas9792@gmail.com>
2025-02-23 22:59:32 +0530

    created addMessage method and defining route in message.js route file and adding styling to chat container default avatar

--- server/routes/message.js
+++
@@ -1,6 +0,0 @@
-const { getMessages } = require("../controllers/messageController");
-const router = require("express").Router();
-
-router.post("/getmsg/", getMessages);
-
-module.exports = router;
