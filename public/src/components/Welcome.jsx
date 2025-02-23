1ab75c3a871858f190c4594e18934a37723942e7
Szkrishna <krisnas9792@gmail.com>
2025-02-23 20:39:22 +0530

    adding user to using useeffect set user async method

--- public/src/components/Welcome.jsx
+++ public/src/components/Welcome.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from "styled-components";
 import Robot from "../assets/robot.gif";
 
-export default function Welcome({currentUser}) {
+export default function Welcome() {
+  const [userName, setUserName] = useState("");
+  useEffect(() => {
+    const setUser = async () => {
+      setUserName(
+        await JSON.parse(
+          localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+        ).username
+      );
+    }
+    setUser();
+  }, []);
   return (
     <Container>
       <img src={Robot} alt="Robot" />
       <h1>
-        Welcome, <span>User</span>
+      Welcome, <span>{userName}!</span>
       </h1>
       <h3>Please select a chat to start Messaging</h3>
     </Container>
