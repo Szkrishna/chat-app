7c5d8d1115d49ae9780056a9e2284bed3124bdcb
Szkrishna <krisnas9792@gmail.com>
2025-02-23 20:33:33 +0530

    adding content to welcome component and styling it

--- public/src/components/Welcome.jsx
+++ public/src/components/Welcome.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from 'react';
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
 
-export default function Welcome() {
+export default function Welcome({currentUser}) {
   return (
-    <div>Welcome</div>
+    <Container>
+      <img src={Robot} alt="Robot" />
+      <h1>
+        Welcome, <span>User</span>
+      </h1>
+      <h3>Please select a chat to start Messaging</h3>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: #4e0eff;
+  }
+`;
